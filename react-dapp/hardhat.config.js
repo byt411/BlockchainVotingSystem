108773111ab6c54d299803aa25c977b40a88dfec
Brian Yim Tam <byt411@hotmail.com>
2021-11-03 12:33:00 +0000

    initial commit

---
+++ react-dapp/hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/41a7a191d3be4189961b3248353c1eb9",
+      accounts: [`0xd4c705603ee106edb264a6163ec2afe9f96cd98de4b6552d929f932e09e07368`]
+    }
+  }
+};
